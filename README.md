2c661dd447a1d0cf3b0002a6513d2acdc87070df
DunoCgame <luisdunocastellano@gmail.com>
2022-06-25 11:03:00 -0400

    UndoRedo update

---
+++ README.md
@@ -0,0 +1,28 @@
+# QuickUndoRedo
+
+Module for the implementation of the function to redo and undo
+
+### functions
+
+### init 
+>new UndoRedo();
+
+Initializes the storage object function which is stored in a variable
+
+let History = new UndoRedo();
+
+### Save
+
+Save function receives the information to be stored
+
+>History.Save(data);
+
+### Undo  and Redo
+
+>History.Redo();  and History.Undo();
+
+These functions return the stored information undo returns the previous information while the redo function returns the most current information stored
+
+## Example
+
+>
--- UndoRedo.js
+++ UndoRedo.js
@@ -3,19 +3,19 @@ function UndoRedo(){
 	let pos=0;
 	let SaveDate=[];
 
-	// Guardado
+	//Guardado
 	this.Save=function(data){
-		if(data!=undefined || data!=null){ SaveDate.push(data); pos = SaveDate.length-1; }
+	 if(data!=undefined || data!=null){ SaveDate.push(data); pos = SaveDate.length-1; }
 		
 	}
 
-	// DESHACER
+	//DESHACER
 	this.Undo=function(){
 			if(pos>0 && SaveDate.length>0){	pos-=1; }
 						return SaveDate[pos];
 	}
 
-	// REHACER 
+	//REHACER 
 	this.Redo=function(){
 			if(pos<(SaveDate.length-1)){ pos+=1;		}
 						return SaveDate[pos];
@@ -23,7 +23,7 @@ function UndoRedo(){
 
 	//Estado
 	this.State=function(){
-		if(SaveDate.length>0){
+		if(SaveDate.length>=0){
 			if(pos!=0){return true; }
 					   else{ return false;	}
 		}
